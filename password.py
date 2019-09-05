0007f808fea4767b0d38c40b9687eeb44a89c363
pascaline <passyish5@gmail.com>
2019-09-05 10:23:00 +0200

    added the users list variable that will be used to store our created users objects

--- password.py
+++ password.py
@@ -1,1 +1,18 @@
 class User:
+    """
+    class that generates and create
+    user instances
+    """
+    list_of_users = []
+
+    def __init__(self, first_name, last_name, user_name, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.password = password
+
+    # def save_user(self):
+    #     '''
+    #     save_user function saves User objects into list_of_users
+    #     '''
+    #     self.list_of_users.append(self)
