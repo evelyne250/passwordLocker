c919b548c093d460a86363e6f3f41c7b6e03d666
pascaline <passyish5@gmail.com>
2019-09-05 11:53:51 +0200

    added the The setUp() method which allows us to define instructions that will be executed before each test method along with the self.assertEqual function

--- credentials_test.py
+++ credentials_test.py
@@ -5,3 +5,21 @@ class TestCredentials(unittest.TestCase):
     '''
     Test class that defines test cases for the credentials class behaviours.
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credentials("evelyne","evelyne@22")
+
+    def test_init(self):
+          '''
+          test_init test case to test if the object is initialized properly
+          '''
+          self.assertEqual(self.new_credential.account_name, "evelyne")
+          self.assertEqual(self.new_credential.account_password, "evelyne@22")
+          
+
+if __name__ == '__main__':
+    unittest.main()
+ 
