f91f57b37b867fbf321e8388e768b337f1a7e998
pascaline <passyish5@gmail.com>
2019-09-05 10:52:20 +0200

    added self.assertEqual() which is a TestCase method that checks for an expected result

--- password_test.py
+++ password_test.py
@@ -11,3 +11,14 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("evelyne","umuhire", "evelyne250", "evelyne@22")
 
+    def test_init(self):
+          '''
+          test_init test case to test if the object is initialized properly
+          '''
+          self.assertEqual(self.new_user.first_name, "evelyne")
+          self.assertEqual(self.new_user.last_name, "umuhire")
+          self.assertEqual(self.new_user.user_name, "evelyne250")
+          self.assertEqual(self.new_user.password, "evelyne@22")
+
+# if __name__ == '__main__':
+#     unittest.main()
